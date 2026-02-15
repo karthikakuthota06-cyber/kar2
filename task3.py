1816fd738c89ae5a3a422005cf83fd0ca5d195ee
karthikakuthota06-cyber <karthikakuthota06@gmail.com>
2026-02-15 21:25:46 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,9 @@
+#Taking input from the user
+n=int(input("Enter a number : "))
+
+if(n%2==0):
+	print(n,"is an even number")   # if the condition is true then prints the number is even number.
+
+else :
+	print(n,"is an odd number")  # if the condition in if control statement is false then prints the number is odd number.
+
---
+++ task4.py
@@ -0,0 +1,7 @@
+# To store the sum of the numbers 
+sum=0
+
+for i in range(1,51):   #The loop will execute upto 1 to 50
+	sum+=i
+
+print("The sum of numbers from 1 to 50 is:",sum)
